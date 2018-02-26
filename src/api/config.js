32e79d4c00b5f8e3a44fa664551f40a24286ee1a
Glorimar <glorimar182@gmail.com>
2018-02-26 18:43:19 -0300

    Commit inicial

---
+++ src/api/config.js
@@ -0,0 +1,13 @@
+export default {
+	appname: 'Platzimusic',
+	apiKey: '910394f54fe625bfaced877f1c5b4689',
+	secret: 'a340edf2fbed545101034a3e7705bf1b',
+	registeredTo: 'glorimar182'
+}
+
+
+
+//Application name	
+//API key	
+//Shared secret	
+//Registered to	
